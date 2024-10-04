9dace75fd604d64a28196048c162b5ddc762aa50
Bao Tran Ha <hbtran2004tnct@gmail.com>
2024-10-04 15:59:25 +0700

    first commit

---
+++ WebApplication2/Controllers/CatalogController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using WebApplication2.Models;
+
+namespace WebApplication2.Controllers
+{
+    public class CatalogController : Controller
+    {
+        // GET: Catalog
+        public ActionResult Index()
+        {
+            DataClasses1DataContext context = new DataClasses1DataContext();
+            List<Catalog> dsCatalog = context.Catalogs.ToList();
+            return View(dsCatalog);
+        }
+    }
+}
---
+++ WebApplication2/Controllers/HelloController.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace WebApplication2.Controllers
+{
+    public class HelloController : Controller
+    {
+        // GET: Hello
+        public ActionResult Index()
+        {
+            return View();
+        }
+        public ActionResult EmlaMVC()
+        {
+            return Content("Hôm nay em học MVC");
+        }
+        public ActionResult HocMVCKhongKho()
+        {
+            ViewBag.ThongDiep = "Hoc MVC kho";
+            return View();
+        }
+
+        public ActionResult HocMVC()
+        {
+            ViewBag.Message = "Hôm nay bùn ngủ qua hà";
+            return View();
+        }
+    }
+}
